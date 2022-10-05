7aa7d7cebd81833b3a497cda35f9b0255b42d335
ellzmarie <elliematutisdigal@gmail.com>
2022-10-05 15:20:58 -0700

    project dummies

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ submitBtn.addEventListener("click", (e) => {
     e.preventDefault()
     console.log(inputBox.value)
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&units=imperial&appid=a900b9c844bb20ffaaf85e81c6058302`)
-//   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputBox.value}&limit={limit}&appid={a900b9c844bb20ffaaf85e81c6058302}`)
     .then((res) => res.json())
     .then((data) => {
     weatherForCity.innerText = "Current City: " + data.name 
